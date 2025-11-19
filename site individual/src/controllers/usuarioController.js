6bd5b8eb8f5ffb3dc2a13a800d391229bf52a209
João Pedro <joao.coimbra@sptech.school>
2025-11-18 21:36:47 -0300

    Subindo codigo dash conectado com bd

--- site individual/src/controllers/usuarioController.js
+++ site individual/src/controllers/usuarioController.js
@@ -17,27 +17,23 @@ function autenticar(req, res) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
-                    // if (resultadoAutenticar.length == 1) {
-                    //     console.log(resultadoAutenticar);
-
-                    //     aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                    //         .then((resultadoAquarios) => {
-                    //             if (resultadoAquarios.length > 0) {
-                    //                 res.json({
-                    //                     email: resultadoAutenticar[0].email,
-                    //                     nome: resultadoAutenticar[0].nome,
-                    //                     senha: resultadoAutenticar[0].senha,
-                    //                     aquarios: resultadoAquarios
-                    //                 });
-                    //             } else {
-                    //                 res.status(204).json({ aquarios: [] });
-                    //             }
-                    //         })
-                    // } else if (resultadoAutenticar.length == 0) {
-                    //     res.status(403).send("Email e/ou senha inválido(s)");
-                    // } else {
-                    //     res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                    // }
+                    if (resultadoAutenticar.length == 1) {
+                        console.log(resultadoAutenticar);
+                       
+                               if (resultadoAutenticar.length > 0) {
+                                    res.json({
+                                       email: resultadoAutenticar[0].email,
+                                       nome: resultadoAutenticar[0].nome,
+                                        senha: resultadoAutenticar[0].senha
+                                   });
+                             } else {
+                                }
+                            
+                 } else if (resultadoAutenticar.length == 0) {
+                   res.status(403).send("Email e/ou senha inválido(s)");
+                 } else {
+                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                     }
                 }
             ).catch(
                 function (erro) {
